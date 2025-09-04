5ccabd8e810a8f5c0d1eff2bffb789db523cbd51
akshay <akshayjha2111@gmail.com>
2025-09-04 23:15:04 +0530

    updating models

--- utils/apiresponse.js
+++ utils/apiresponse.js
@@ -1,10 +1,10 @@
 //a class of apiresponse with defined objects
 class apiresponse {
-    constructor(statuscode,data,message="success"){
-        this.statuscode=statuscode;
-        this.data=data;
-        this.message=message;
-        this.success=statuscode<400;
-    }
+  constructor(statuscode, data, message = "success") {
+    this.statuscode = statuscode;
+    this.data = data;
+    this.message = message;
+    this.success = statuscode < 400;
+  }
 }
-export {apiresponse}
+export { apiresponse };
--- utils/asynchandler.js
+++ utils/asynchandler.js
@@ -1,9 +1,6 @@
-const asynchandler= (requesthandler)=>{
-    return(req,res,next)=>{
-        Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err))
-    }
-}
-export
-{
-    asynchandler
-}
+const asynchandler = (requesthandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requesthandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asynchandler };
