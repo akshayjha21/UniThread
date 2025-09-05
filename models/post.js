a01381cab4f28ece6630215e4d2f3532522ac4a0
akshay <akshayjha2111@gmail.com>
2025-09-05 20:01:50 +0530

    adding files

---
+++ models/post.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const PostLike=new mongoose.Schema({
+    postId:{
+        type:mongoose.Types.ObjectId,
+        ref:"post",
+        required:true,
+    },
+    userId:{
+        type:mongoose.Types.ObjectId,
+        ref:"user",
+        required:true,
+    },
+},
+{timestamps:true});
+
+module.exports = mongoose.model("PostLike",PostLike);
