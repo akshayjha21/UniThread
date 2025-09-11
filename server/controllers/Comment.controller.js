06be0c14a192673210d9fc4846a6c5441eea8fa1
akshay <akshayjha2111@gmail.com>
2025-09-11 20:12:06 +0530

    updated the comment controller

--- server/controllers/Comment.controller.js
+++ server/controllers/Comment.controller.js
@@ -9,6 +9,7 @@ import { apiresponse } from "../utils/apiresponse.js";
 
 const cooldown=new Set();
 
+//create comment
 const createComment=asynchandler(async(req,res)=>{
     try {
         const postid=req.params.id
@@ -42,6 +43,133 @@ const createComment=asynchandler(async(req,res)=>{
     }
 })
 
+//get comment 
+
+const getPostComment=asynchandler(async(req,res)=>{
+    try {
+        const postid=req.params.id
+        const comments=await Comment.findById({post:postid})
+        .populate("commenter","-password")
+        .sort("createdAt")
+
+        if(!postid){
+            throw new apierror(404,"Post not found")
+        }
+        if(!comments){
+            throw new apierror(400,"Comment not found on the post")
+        }     
+        const commentParents={};
+        const rootComments=[];
+        
+        //getting comment parents
+        for(let i=0;i<comments.length;i++){
+            let comment=comments[i];
+            commentParents[comment.id]=comment
+        }
+
+        for(let i=0;i<comments.length;i++){
+            const comment=comments[i];
+            if(comment.parent){
+                let commentParent=commentParents[comment.parent]
+                commentParent.children=[...commentParent.children,comment]
+            }
+            else{
+                rootComments = [...rootComments, comment];
+            }
+        }
+        return res.status(200).json(new apiresponse(200,{PostComment:rootComments}))
+        
+    } catch (error) {
+        throw new apierror(500,"Something went wrong in getPostComment")
+    }
+})
+
+//get UserComment 
+
+const getUserComment=asynchandler(async(req,res)=>{
+    try {
+        const userId=req.params.id
+        let {page,sortBy}=req.query
+        
+        if(!sortBy){
+            sortBy="-createdAt"
+        }
+        if(!page){
+            page=1;
+        }
+         let comments = await Comment.find({ commenter: userId })
+      .sort(sortBy)                      
+      .populate("post"); 
+      if(!comments)      {
+        throw new apierror(400,"Comment doesn't exist")
+      }   
+      return res.status(200).json(new apiresponse(200,{UserComment:comments}))         
+    } catch (error) {
+        throw new apierror(500,"Something went wrong while getting user comment")
+    }
+})
+
+//deleteComment 
+
+const deleteComment=asynchandler(async(req,res)=>{
+    try {
+        const commentId=req.params.id;
+        const {userId,isAdmin}=req.body
+
+        const comment=await Comment.findById(commentId);
+
+        if(!commentId){
+            throw new apierror(400,"Comment not found")
+        }
+        if(comment.commenter!=userId && !isAdmin){
+            throw new apierror(400,"Not authorized to delete comment")
+        }
+        await comment.remove();
+
+        const post = await Post.findById(comment.post);
+
+        post.commentCount = (await Comment.find({ post: post._id })).length;
+
+        await post.save();
+        return res.status(200).json(new apiresponse(200,{comment:comment}))
+    } catch (error) {
+        throw new apierror(500,"Something went wrong while deleting error")
+    }
+})
+
+const updateComment = asynchandler(async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const { userId, content, isAdmin } = req.body;
+
+    if (!content) {
+      throw new apierror(400,"All input required");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      throw new apierror(400,"Comment not found");
+    }
+
+    if (comment.commenter != userId && !isAdmin) {
+      throw new apierror(400,"Not authorized to update comment");
+    }
+
+    comment.content = content;
+    comment.edited = true;
+    await comment.save();
+
+    return res.status(200).json(new apiresponse(200,{comment:comment}));
+  } catch (err) {
+    throw new apierror(500,"Something went wrong updating the comment ")
+  }
+});
+
 export{
-    createComment
+    createComment,
+    getPostComment,
+    getUserComment,
+    deleteComment,
+    updateComment
 }
